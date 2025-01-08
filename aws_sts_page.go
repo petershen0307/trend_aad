237ca0dd703372ec33719f3748e285e38b5bb1ce
Peter Shen <petershen0307@hotmail.com>
2025-01-08 14:26:29 +0800

    embedded logrus to rod logger and remove leakless flag

--- aws_sts_page.go
+++ aws_sts_page.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"html"
 	"io"
-	"log"
 	"runtime"
 	"strings"
 	"time"
@@ -22,13 +21,14 @@ const trendAwsStsUrl = "https://awssts.infosec.trendmicro.com"
 func InitialBrowser() *rod.Browser {
 	controlURL := ""
 	if path, exists := launcher.LookPath(); exists {
-		log.Println("detect browser", path)
-		controlURL = launcher.New().Bin(path).Headless(true).Leakless(false).MustLaunch()
+		logrus.Info("detect browser", path)
+		controlURL = launcher.New().Bin(path).Headless(true).MustLaunch()
 	} else {
 		// try to install chromium
+		logrus.Info("install browser")
 		controlURL = launcher.New().Headless(true).MustLaunch()
 	}
-	browser := rod.New().ControlURL(controlURL).MustConnect()
+	browser := rod.New().ControlURL(controlURL).MustConnect().Logger(logrus.StandardLogger())
 	launcher.Open(browser.ServeMonitor(""))
 	runtime.SetFinalizer(browser, func(browser *rod.Browser) {
 		browser.MustClose()
