49f95724af62435c478ca621ca82e18d795aad3d
Peter Shen <petershen0307@hotmail.com>
2025-01-09 17:00:19 +0800

    don't use system chrome to open the server monitor page
    launcher.Open(browser.ServeMonitor("")) this line use the system chrome to open server monitor page
    Leakless(true) feature is preventing go program stop but the sub-process wouldn't be stop
    but leakless will trigger a middle process to do this thing, this middle process will trigger antivirus alert
    
    refactor use the same launcher settings in install chrome by rod and system chrome

--- browser.go
+++ browser.go
@@ -10,18 +10,19 @@ import (
 
 func InitialBrowser() *rod.Browser {
 	controlURL := ""
+	l := launcher.New().Headless(true).Leakless(false)
 	if path, exists := launcher.LookPath(); exists {
 		logrus.Info("detect browser", path)
-		controlURL = launcher.New().Bin(path).Headless(true).MustLaunch()
+		controlURL = l.Bin(path).MustLaunch()
 	} else {
 		// try to install chromium
 		logrus.Info("install browser")
-		controlURL = launcher.New().Headless(true).MustLaunch()
+		controlURL = l.MustLaunch()
 	}
 	browser := rod.New().ControlURL(controlURL).MustConnect().Logger(logrus.StandardLogger())
-	launcher.Open(browser.ServeMonitor(""))
 	runtime.SetFinalizer(browser, func(browser *rod.Browser) {
 		browser.MustClose()
 	})
+	logrus.Debugf("server monitor url: %s", browser.ServeMonitor(""))
 	return browser
 }
